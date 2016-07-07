7aa7bc4f86b2df43796149169d60d2801221e66b
aweffr <aweffr@qq.com>
2016-07-07 15:51:03 +0800

    abstract the function of "tell whether it is a innerPoint"
    combine the step of "get raw data" and "classify it" into one step.
    
    the innerPoint format is [original,original,original,deformed,deformed,deformed]

--- terminal/odbProcessor.py
+++ terminal/odbProcessor.py
@@ -1,35 +1,77 @@
 # -*- coding:utf-8 -*-
-# fileName: odbProcessor-II.py
+# odbProcessor.py
+# 本脚本用于得到边界点和内部点的后处理（位移）数据。
+# 对于边界点，数据格式为np.array(), (original, original, deformed)
+# 对于内部点，数序格式为np.array(), (deformed, deformed, deformed)
+
 import shelve
 from odbAccess import *
-from numpy import array, array2string, std
-def odbNodeDeformation(odbPath = 'D:/My Docs/AbaqusTemp/FRPGrid-.odb',outPath = r'D:\abaqus_execpy\outdata\coord.txt'):
+from numpy import array, array2string, std, zeros
+
+
+def isSamePoint(point1, point2, err=0.0002):
+    # 输入两个点，判断两个点平面上是不是同一个点
+    if abs(point1[0] - point2[0]) < err and abs(point1[1] - point2[1]) < err:
+        return True
+    else:
+        return False
+
+def isInnerPoints(point, innerPointLst, err=0.0002):
+    # point -> 判断改点算不算内部点
+    # innerPointLst -> 模型文件中内部点的坐标
+
+
+    modelNodeFile.close()
+
+def odbNodeDeformation(odbPath = 'D:/My Docs/AbaqusTemp/FRPGrid-.odb',
+    outFilePath=r'D:\abaqus_execpy\outdata\coord.txt',
+    modelFilePath=''):
     # odbPath -> abaqus目标后处理数据库odb的文件位置
     # outPath -> 将读取的结果输入到的shelve的位置
+    # modelFilePath -> 模型变形前坐标
     odb = openOdb(path=odbPath)
+
+    # 初始化模型坐标中的数据
+    modelNodeFile = shelve.open(modelFilePath)
+    xBoundPoints = modelNodeFile['xCoordPoints']
+    yBoundPoints = modelNodeFile['yCoordPoints']
+    InnerPoints = modelNodeFile['inCoordPoints']
+
+    # abaqus中的API模块，提取最终变形的data.
     finalFrame = odb.steps['Step-1'].frames[-1]
-    f = shelve.open(outPath)
-    out_points = []
     print 'finalFrame Get!'
+    
+    outFile = shelve.open(outFilePath)
+
+    innerPoints_Deformed = []
+    boundPoints_Deformed = []
+
     for value in finalFrame.fieldOutputs['U'].values:
-        # print 'Working in loop!'
-        nodeLabel = value.nodeLabel
-        # print "Now Check the instance:"
-        # print value.instance
+        # 当模型中存在connector时该instance为None,需要跳过
         if value.instance == None:
-        	continue
+            continue
+        nodeLabel = value.nodeLabel
         checkLabel = value.instance.nodes[value.nodeLabel-1].label
         if nodeLabel == checkLabel:
             deformationVector = array(value.data)
             orignalCoordinate = array(value.instance.nodes[value.nodeLabel-1].coordinates)
-            orignalCoordinate[-1] = deformationVector[-1]
-            # orignalCoordinate的数据结构变成（x,y,z），x,y为平面上坐标的点，z为位移的变化量
-            out_points.append(orignalCoordinate)
+            # 此处判断该点是否为边界点
+            if isInnerPoints(orignalCoordinate, InnerPoints):
+                tempArray = zeros(6, float)
+                tempArray[:3] = orignalCoordinate
+                tempArray[4:] = deformationVector
+                innerPoints_Deformed.append(tempArray)
+            else:
+                orignalCoordinate[-1] = deformationVector[-1]
+                # orignalCoordinate的数据结构变成（x,y,z），x,y为平面上坐标的点，z为位移的变化量
+                boundPoints_Deformed.append(orignalCoordinate)
         else:
             print 'Wrong point, Label is %d'%nodeLabel
-    f["out_points"] = out_points
-    print 'Raw Data Getter Complete!'
-    f.close()
+
+    outFile['Bound_Points'] = boundPoints_Deformed
+    outFile['Inner_Points'] = innerPoints_Deformed
+    print 'Odb data classfication Complete!'
+    outFile.close()
 
 def NodesFilter(shelveFileLocation, modelFilePath, outFilePath, accuracy = 0.001):
     pointInfo = shelve.open(shelveFileLocation)
@@ -69,6 +111,7 @@ def NodesFilter(shelveFileLocation, modelFilePath, outFilePath, accuracy = 0.001
     print "Raw data processing complete!"
     print "File is saved at %s"%outFilePath
 
+    # To compute the standard error of z direction deformation.
     temp_z = []
     for point in tempBound:
     	temp_z.append(point[2])
