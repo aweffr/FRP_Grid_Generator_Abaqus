49e2126905e81b7fb9ad1d4ea922ef402f6e8bd6
aweffr <aweffr@qq.com>
2016-07-07 17:17:49 +0800

    complete the script for point coordination of the third layer beam.

--- ThirdBeamLayer.py
+++ ThirdBeamLayer.py
@@ -3,12 +3,35 @@
 
 # 本脚本的运行环境为Anaconda, conda 4.1.4。
 # 本脚本用于生成坐标数据，第三层（斜向）杆件的连接坐标点数据。
-# 坐标数据为np.array格式:(deformed(float), deformed(float), deformed(float))
-# 算法：
-# 导入模型坐标文件(shelve格式)，modelPoints-XX.dat, 其中注意InnerPoints
-# 导入后处理坐标文件(shelve格式)，washedPoints-XX.dat, 其中包括InnerPoints
-# 得到内部点在变形后x,y,z的单精度坐标
 
-import handytools as hd
 import numpy as np
 import shelve
+import operator
+
+def classification(inlst,):
+	# 利用其原坐标处于一条直线上进行分类
+    outDir = {}
+    for point in inlst:
+        tempValue = point[1] - point[0]
+        if outDir.has_key(tempValue):
+            outDir[tempValue].append(point)
+        else:
+            outDir[tempValue] = [point,]
+    return outDir
+
+odbDeformationData = shelve.open("washedPoints-Closer-Multi3-11.dat")
+
+Inner_Points = f["Inner_Points"]
+Inner_Points = sorted(Inner_Points, key=operator.itemgetter(0,1))
+Inner_Points = Inner_Points[::2]
+
+classifiedDict = classification(Inner_Points,)
+
+odbDeformationData.close()
+
+out = shelve.open("thirdBeam.dat")
+out["log"] = "key word: 'pointDict'."
+out["pointDict"] = classifiedDict
+out.close()
+
+
